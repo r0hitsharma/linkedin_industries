c8028e375ef84f63d39c97c460389876a0ec1e9b
Jeff Lee <jeffomatic@gmail.com>
2012-07-26 21:40:56 -0400

    - Finish exporter script and export json, yaml, rb - Update README

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 linkedin_industries
 ===================
 
-A list of LinkedIn industries, in various formats
+A list of LinkedIn industries, in various formats.
+
+The LinkedIn API does not appear to provide an endpoint for retrieving a complete list of industries. If you want a list for your app, you'll need to keep a list handy in your local codebase. And thus.
+
+`linkedin_industries.csv` is pulled from [LinkedIn's official list](http://developer.linkedin.com/documents/industry-codes) and then exported to JSON, YAML, and Ruby source.
+
+The most recent version of this list is [available on Github](https://github.com/jeffomatic/linkedin_industries).
---
+++ generate.rb
@@ -0,0 +1,42 @@
+require 'yaml'
+require 'json'
+require 'pp'
+
+# Some input/output file paths
+infile = File.join(File.dirname(__FILE__), 'linkedin_industries.csv')
+outfile_basename = 'linkedin_industries'
+
+# Grab input from CSV and covert to native Ruby objects. We use CSV here because
+# it's the most direct text format we can get by copying from LinkedIn's own
+# docs.
+industries = File.open(infile).read
+  .strip.split(/\r?\n/)
+  .inject [] do |memo, line|
+  # Extract comma-separated components
+  cols = line.split(',').map{ |c| c.strip }
+
+  # Skip poorly-formatted lines
+  next memo unless cols.size == 3
+
+  # Add industry details
+  memo << {
+    'code' => cols[0].to_i,
+    'groups' => cols[1].split(/\s+/),
+    'description' => cols[2],
+  }
+
+  # Next line
+  memo
+end
+
+# Dump to various formats
+{
+  # JSON
+  'js' => JSON.pretty_generate(industries),
+  'yaml' => industries.to_yaml,
+  'rb' => industries.pretty_inspect
+}.each do |extension, text|
+  filename = "#{outfile_basename}.#{extension}"
+  puts "generating #{filename}"  
+  File.open(filename, 'w') { |f| f.write(text) }
+end
